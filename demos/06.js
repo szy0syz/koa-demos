9af4b4b2f1f22ad101cac226071e8066c18ff819
szy0syz <szy0syz@gmail.com>
2018-01-10 21:30:44 +0800

    add demo 05 06

---
+++ demos/06.js
@@ -0,0 +1,18 @@
+// demos/02.js
+const Koa = require('koa')
+const route = require('koa-route')
+const app = new Koa()
+
+const about = ctx => {
+  ctx.response.type = 'html'
+  ctx.response.body = '<a href="/">About: Index Page</a>'
+}
+
+const main = ctx => {
+  ctx.response.type = 'html'
+  ctx.response.body = 'Hello World'
+}
+
+app.use(route.get('/', main))
+app.use(route.get('/about', about))
+app.listen(3001)
